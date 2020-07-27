ffd63c93174b374f1d218125b4488beeebef1291
[vishal035] <[kumarvishal880288@gmail.com]>
2020-07-28 00:26:31 +0530

    Alerting the user for non-supported screen size

---
+++ script/app.js
@@ -0,0 +1,12 @@
+const bootbox = require('./bootbox.min');
+var width = window.innerWidth || document.documentElement.clientWidth ||
+    document.body.clientWidth;
+var height = window.innerHeight || document.documentElement.clientHeight ||
+    document.body.clientHeight;
+
+if (!(width >= 900)) {
+    // document.getElementsByTagName('body').style.backgroundColor = "yellow";
+    bootbox.alert("Hello world!");
+    console.log(document.getElementsByTagName('html'));
+    window.stop();
+}
