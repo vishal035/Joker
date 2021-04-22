c36e8f240b535ea191b8f67bd726e079cbede653
vishal035 <kumarvishal880288@gmai;.com>
2021-04-22 12:09:44 +0530

    Re-Working is needed

--- script/app.js
+++ script/app.js
@@ -1,12 +1,16 @@
-// const bootbox = require('./bootbox.min');
-// var width = window.innerWidth || document.documentElement.clientWidth ||
-//     document.body.clientWidth;
-// var height = window.innerHeight || document.documentElement.clientHeight ||
-//     document.body.clientHeight;
+const bootbox = require('./bootbox.min');
+var width =
+  window.innerWidth ||
+  document.documentElement.clientWidth ||
+  document.body.clientWidth;
+var height =
+  window.innerHeight ||
+  document.documentElement.clientHeight ||
+  document.body.clientHeight;
 
-// if (!(width >= 900)) {
-//     // document.getElementsByTagName('body').style.backgroundColor = "yellow";
-//     bootbox.alert("Hello world!");
-//     console.log(document.getElementsByTagName('html'));
-//     window.stop();
-// }
+if (!(width <= 900)) {
+  // document.getElementsByTagName('body').style.backgroundColor = "yellow";
+  bootbox.alert('Hello world!');
+  console.log(document.getElementsByTagName('html'));
+  window.stop();
+}
